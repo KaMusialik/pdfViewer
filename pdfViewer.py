e8d320c0e9ef32ac8610d378661963f7e6da414a
karol <km21166@gmail.com>
2024-08-10 09:08:00 +0200

    20240810

---
+++ pdfViewer.py
@@ -0,0 +1,58 @@
+import sys
+import subprocess
+import os
+
+from PyQt5.QtWidgets import QFileDialog, QApplication
+
+
+def zeigePDF(filename):
+
+    if not filename:
+        print("keine Datei festgelegt!!!")
+        sys.exit(0)
+
+    if pruefeObDateiExistiert(filename) == 1:
+        print('Datei existiert nicht. Abbruch')
+        sys.exit(0)
+
+    oefnePDF(filename)
+
+def pruefeObDateiExistiert(filename):
+    if os.path.isfile(filename):
+        print('pruefeObDateiExistiert: Datei existiert.')
+        return 0
+    else:
+        print('pruefeObDateiExistiert: Datei existiert nicht.')
+        return 1
+
+
+def oefnePDF(filename):
+
+    if sys.platform == "Windows":
+        os.startfile(filename)
+    else:
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, filename])
+
+
+def waehleEinPDF():
+
+    app = QApplication(sys.argv)
+    filename, _ = QFileDialog.getOpenFileName(filter="PDF (*.pdf)")
+
+    return str(filename)
+
+if __name__ == "__main__":
+
+    anzahlArrgumente = len(sys.argv)
+    if anzahlArrgumente == 1:  # keine Arrgumente übergeben
+        print('keine Arrgumente übergeben')
+        filename = waehleEinPDF()
+    elif anzahlArrgumente == 2:  # genau eine Datei Übergeben
+        print('genau eine Datei übergeben')
+        filename = sys.argv[1]
+    else:  # meherere dateine übergeben
+        print('Es wurden mehrere Dateien übergeben. Es wird aber nur eine erwartet. Daher Abbruch!')
+        sys.exit(0)
+
+    zeigePDF(filename)
